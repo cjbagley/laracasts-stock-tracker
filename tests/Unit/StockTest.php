cf0f35bc45f5d2e999d40a8b9442c9dc888b3c66
Colin Bagley <cjbagley@googlemail.com>
2024-09-26 11:02:00 +0100

    feat: Tracking command and functionality
    Add test for saving stock

--- tests/Unit/StockTest.php
+++ tests/Unit/StockTest.php
@@ -1,9 +1,37 @@
 <?php
 
+use App\Clients\Client;
+use App\Clients\ClientException;
+use App\Clients\StockStatus;
+use App\Models\Retailer;
+use App\Models\Stock;
+use Database\Seeders\RetailerWithProductSeeder;
+use Facades\App\Clients\ClientFactory;
+
 it('throws exception when no client found when tracking', function () {
-    $this->seed(\Database\Seeders\RetailerWithProductSeeder::class);
+    $this->seed(RetailerWithProductSeeder::class);
 
-    \App\Models\Retailer::first()->update(['name' => 'Foo Retailer']);
+    Retailer::first()->update(['name' => 'Foo Retailer']);
 
-    \App\Models\Stock::first()->track();
-})->throws(\App\Clients\ClientException::class);
+    Stock::first()->track();
+})->throws(ClientException::class);
+
+it('updates local stock status after tracking', function () {
+    $this->seed(RetailerWithProductSeeder::class);
+
+    ClientFactory::shouldReceive('make')->andReturn(new FakeClient);
+
+    /** @var Stock $stock */
+    $stock = tap(Stock::first())->track();
+
+    expect($stock->in_stock)->toBeTrue()
+        ->and($stock->price)->toBe(9900);
+});
+
+class FakeClient implements Client
+{
+    public function checkAvailability(): StockStatus
+    {
+        return new StockStatus(available: true, price: 9900);
+    }
+}
